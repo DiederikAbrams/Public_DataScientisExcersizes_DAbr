37b13ab92269f7a0ecb8ebfbd1e1b95b89475c6b
diederikabrams <diederik.abrams@gmail.com>
2024-09-25 19:47:45 +0200

    Aanpassing 2

--- try_out_test.py
+++ try_out_test.py
@@ -2,3 +2,5 @@ print ("deze code komt van github, test")
 print ("import gelukt")
 #extra code van pycharm
 print("deze code komt van pycharm")
+#commit sessie 2
+print("deze code komt van pycharm sessie 2")
